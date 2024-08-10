ce9a284e3c71f863fd06608f0c30fb7453642b19
pberd21 <berdnikov0500@gmail.com>
2024-08-10 16:01:04 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 3, "c", "h"
+print("Immutable tuple:", immutable_var)
+mutuble_list = [1,4,"d","y"]
+mutuble_list[2]= "modified"
+mutuble_list.append(3)
+print("Mutuble list:", mutuble_list)
+
