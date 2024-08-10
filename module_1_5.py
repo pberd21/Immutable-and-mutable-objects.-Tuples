6e2015859486e8544f51ed051fc16d84d84c305b
pberd21 <berdnikov0500@gmail.com>
2024-08-10 16:03:18 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -4,4 +4,4 @@ mutuble_list = [1,4,"d","y"]
 mutuble_list[2]= "modified"
 mutuble_list.append(3)
 print("Mutuble list:", mutuble_list)
-
+#Кортежи не изменяемы, поэтому изменение их елементов невозможно.
